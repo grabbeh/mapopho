e3fccca9b115acc56d08753daa0c63080a511431
Michael <mbg@outlook.com>
2013-10-25 14:10:58 +0100

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -1,39 +1,23 @@
 var FlickrAPI = require('../flickrnode/lib/flickr').FlickrAPI
 , sys = require('sys')
 , api = require('../config/api.js')
-, flickr = new FlickrAPI(api.details.key);
+, flickr = new FlickrAPI(api.details.key)
+, geocoder = require('geocoder');
 
 exports.home = function(req, res){
   res.render('home');
 };
 
 exports.api = function(req, res){
-	function searchObject(obj) {
-		this.lat = obj.lat;
-		this.lon = obj.lon;
-		this.license = obj.licenses;
-		this.min_date_upload = 946706400;
-		this.tags = obj.tag;
-	}
-
-	var sO = new searchObject(req.body);
-	flickr.photos.search(sO, function(error, results) {
-		if (error){
-			res.status(500);
-			res.send();
-		}
-		else {
-		if (results.pages === 0){
-			res.status(204);
-			res.send();
-		}
-		else {
-			res.json(results);
-		}
-		}
-		
-	});
-}
+     randomLandBasedCoordinates(2, function(coords){ 
+     	coords.forEach(function(arr){
+     	    var flickrSearchOptions = new searchObject(req.body.tag, arr);
+	    searchFlickr(flickrSearchObject, function(results){
+	    	res.json(results);
+	    });	
+     	})
+    })
+})
 
 exports.privacy = function(req, res){
   res.render('privacy');
@@ -46,4 +30,51 @@ function isEmpty(obj) {
     }
     return true;
 }
+
+function searchFlickr(object, fn) {
+  flickr.photos.search(object, function(error, results) {
+		if (error){
+			searchFlickr(object, fn);
+		}
+		else {
+		     if (results.pages === 0){
+		          searchFlickr(object, fn);
+		      }
+		      else {
+			   fn(results);
+		      }
+		}
+	});
+}
+
+function randomlandBasedCoordinates(number, fn) {
+	
+        var lat = (Math.random() * 180 - 90);
+        var lon = (Math.random() * 360 - 180);
+	var randomCoordinate = [lat, lon];
+	
+	geocoder.reverseGeocode(lat, lon, function(err, results){
+	    if (status == "ZERO_RESULTS") {
+            randomLandBasedCoords(number,fn);
+        } else if (status == "OK" && results) {
+            var landCoordinate = randomCoordinate;
+            coords.push(landCoordinate);
+            if (coords.length < number) {
+                randomLandBasedCoords(number,fn);
+            } else {
+                return fn(coords);
+            }
+        }
+    });		
+}
+
+function flickrSearchOptions(obj, arr) {
+		this.lat = arr[0];
+		this.lon = arr[0];
+		this.min_date_upload = 946706400;
+		this.tags = tag;
+		this.per_page = 2;
+		this.page = 1;
+	}
+
 
