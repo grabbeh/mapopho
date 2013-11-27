0d4a9bb4c4188bdbd271e4a55ea65f476036c870
grabbeh <mbg@outlook.com>
2013-11-27 23:46:09 +0000

    Filter

--- public/controllers/controllers.js
+++ public/controllers/controllers.js
@@ -188,7 +188,7 @@ appModule.controller("mapController", ['$scope', '$location', '$routeParams', '$
         for (var key in copy){
             var obj = copy[key];
             console.log("Looping")
-            console.log("Single object ranking = " + obj.ranking)
+            console.log("Single object ranking = " + obj[ranking])
             var ranking = Number(obj.ranking);
             if (ranking >= pcent){
                fresh[key] = obj;
